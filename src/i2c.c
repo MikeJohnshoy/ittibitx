c854b3b0b8a5af0a4abf16ba881aabe91377fa8e
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-08-20 07:13:45 -0400

    rename file

--- src/i2c.c
+++ src/i2c.c
@@ -1,5 +1,6 @@
-/*
- * i2c.c - I2C access for the si5351 (and, once wired in, the board-ID
+/* i2c.c
+ *
+ * I2C access for the si5351 (and, once wired in, the board-ID
  * EEPROM and INA260 power monitor).
  *
  * This used to bit-bang I2C directly over GPIO from userspace. That
