2240d5434a25368f644d1c389df1da84074add99
MikeJohnshoy <mike.johnshoy@gmail.com>
2026-08-28 15:10:46 -0400

    Add sound mixer functionality to radio.c

--- src/radio.c
+++ src/radio.c
@@ -1,8 +1,9 @@
-// radio.c - see radio.h
+// radio.c
 
 #include "radio.h"
 #include "radio_hw.h"
 #include "si5351.h"
+#include "sound.h"
 #include <stdio.h>
 #include <unistd.h>     // usleep() - relay-settling time, not GPIO access
 
@@ -13,6 +14,10 @@ int in_tx = 0;
 // hardware.  Users can set there own value in hw_settings.ini
 int bfo_freq = 40035000;
 struct vfo lo;
+
+// "Master" gates the WM8731's whole analog output path - the same
+// DAC/output-mixer chain that carries CW (and any future TX) audio out
+#define TX_MASTER_VOL 70
 
 void radio_tune_to(uint32_t f) {
     freq_hdr = f;
@@ -28,7 +33,9 @@ void radio_set_tx(int tx_on) {
         radio_hw_set_ptt(1);
         usleep(20000);              // let PTT assert before keying the relay
         radio_hw_set_tx_relay(1);
+        sound_mixer("hw:0", "Master", TX_MASTER_VOL); // feed the exciter
     } else {
+        sound_mixer("hw:0", "Master", 0); // mute before dropping the relay
         radio_hw_set_ptt(0);
         usleep(5000);               // let the relay settle before dropping PTT
         radio_hw_set_tx_relay(0);
